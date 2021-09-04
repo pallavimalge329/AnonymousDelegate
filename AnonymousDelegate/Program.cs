8c4e9b5d9ae8e0fc69f44f72ebbad113abc64f8a
HP <HP@DESKTOP-N5QBMVO>
2021-09-04 19:08:21 +0530

    Add project files.

---
+++ AnonymousDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousDelegate
+{
+    
+    class Program
+    {
+        public delegate void DelegateSquare(int n);
+        //delegate void DelegateSquare(int n);
+        // no any method for the delegate
+        DelegateSquare ds = delegate (int x)
+        {
+            Console.WriteLine(x * x);
+        };
+    
+        static void Main(string[] args)
+        {
+            DelegateSquare ds = delegate (int x)
+            {
+                Console.WriteLine(x * x);
+            };
+
+            ds(4);
+            Console.ReadKey();
+        }
+
+    }
+}
